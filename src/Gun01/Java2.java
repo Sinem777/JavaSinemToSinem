7187ee46d1db66dead199ebfc495537b8f4ad712
Ali <alikilinc.wk@gmail.com>
2024-08-24 22:38:16 +0300

    task 4 yapıldı

---
+++ src/Gun01/Java2.java
@@ -0,0 +1,10 @@
+package Gun01;
+
+public class Java2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World 1");
+        System.out.println("Hello World 2");
+        System.out.println("Hello World 3");
+        System.out.println("Hello World 4");
+    }
+}
